f9eac2edcadd4ea31b8ec9e2239dcb2220862f7a
Wilson Paiva <paiva.wilson@dedalus.com>
2023-05-24 19:20:19 -0300

    Add prisma

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,12 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server'
 
-type Data = {
-  message: string
-}
-
-export async function GET(
-  req: NextRequest
-) {
-  return NextResponse.json({ hello: 'Next.js' });
+export async function GET(req: NextRequest) {
+  return NextResponse.json({ hello: 'Next.js' }, { status: 200 })
 }
